ff1a426d235ac3eb1fde8a388fd642224afd9fee
Bruce-Yu2017 <ilywanzi@gmail.com>
2018-05-21 23:16:21 -0700

    new

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,5 +3,7 @@ var path = require('path');
 
 
 module.exports = function(app){
-    
+    app.all("*", function (req, res) {
+      res.sendFile('index.html', { root: './client/dist' });
+    })
 }
